5dcb0fa8b7d4309ca184416ae6ea425bf48c7844
anamenici <anamenici@gmail.com>
2022-02-28 14:15:01 -0300

    Upload Ex05 Soma dos numeros do teclado - Do While

--- Exercicios_LacosRepeticao/Exercicio05.java
+++ Exercicios_LacosRepeticao/Exercicio05.java
@@ -1,11 +1,22 @@
 package Exercicios_LacosRepeticao;
 
+import java.util.Scanner;
+
 public class Exercicio05 {
 
 	public static void main(String[] args) {
 
-
+		int numero, soma=0;
+		Scanner leia = new Scanner(System.in);
 		
+		do {
+			System.out.println("\nDigite um número do teclado: ");
+			numero = leia.nextInt();
+			soma += numero;
+		}
+		while(numero!=0);
+		
+		System.out.println("\nA soma dos números digitados é: "+soma);
 		
 		/*
 		 * Crie um programa que leia um número do teclado até que encontre um número 
