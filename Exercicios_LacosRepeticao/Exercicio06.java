859fff8c9627e85a15fef54a48bceb794b00933f
anamenici <anamenici@gmail.com>
2022-02-28 14:24:32 -0300

    Upload Ex06 Soma dos múltiplos de 3 - Do While

--- Exercicios_LacosRepeticao/Exercicio06.java
+++ Exercicios_LacosRepeticao/Exercicio06.java
@@ -1,10 +1,25 @@
 package Exercicios_LacosRepeticao;
 
+import java.util.Scanner;
+
 public class Exercicio06 {
 
 	public static void main(String[] args) {
+		int numero, soma=0;
 		
+		Scanner leia = new Scanner(System.in);
 		
+		do
+		{
+			System.out.println("\n(Digite 0 para sair)\nDigite um número inteiro: ");
+			numero = leia.nextInt();
+			if(numero%3==0)
+			{
+				soma+=numero;
+			}
+		}
+		while(numero!=0);
+		System.out.println("\nA soma dos multiplos de 3 é: "+soma);
 		
 		/*
 		 * Escrever um programa que receba vários números inteiros no teclado. E no final imprimir 
