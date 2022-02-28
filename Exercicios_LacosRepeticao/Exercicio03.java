92989c96db10e8cad853f37193017c2ace1e2c4f
anamenici <anamenici@gmail.com>
2022-02-28 11:07:08 -0300

    Upload Ex03 - While

--- Exercicios_LacosRepeticao/Exercicio03.java
+++ Exercicios_LacosRepeticao/Exercicio03.java
@@ -1,10 +1,34 @@
 package Exercicios_LacosRepeticao;
 
+import java.util.Scanner;
+
 public class Exercicio03 {
 
 	public static void main(String[] args) {
-
-
+	int age, less21=0, plus50=0;
+	
+	Scanner leia = new Scanner(System.in);
+	
+	System.out.println("\nDigite a idade: ");
+	age = leia.nextInt();
+	
+	while(age!=-99)
+	{
+		if(age<21) 
+		{
+			less21++;
+		}
+		else if(age>50)
+		{
+			plus50++;
+		}
+		System.out.println("\nDigite a idade: ");
+		age = leia.nextInt();
+	}
+	
+	System.out.printf("\nResultado: \n%d pessoa(s) menor(es) de 21 anos; "
+			+ "\n%d pessoa(s) maior(es) de 50 anos.", less21, plus50);
+	
 		
 		
 		
